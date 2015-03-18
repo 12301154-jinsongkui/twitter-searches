029c9190e91082950f02eb602d7d957d8fd02bb2
12301154-jinsongkui <12301154@bjtu.edu.cn>
2015-03-18 11:08:01 +0800

    JeremyKim-12301154

--- app/src/main/java/com/deitel/twittersearches/MainActivity.java
+++ app/src/main/java/com/deitel/twittersearches/MainActivity.java
@@ -119,7 +119,9 @@ public class MainActivity extends ListActivity
       // if tag is new, add to and sort tags, then display updated list
       if (!tags.contains(tag))
       {
-         tags.add(tag); // add new tag
+          String urlString = getString(R.string.searchURL) +
+                  Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+          tags.add(urlString); // add new tag
          Collections.sort(tags, String.CASE_INSENSITIVE_ORDER);
          adapter.notifyDataSetChanged(); // rebind tags to ListView
       }
